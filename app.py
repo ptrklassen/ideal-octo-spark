494730e000dcc010e9a9d0c6140b936f6f25b56b
ptrklassen <55111418+ptrklassen@users.noreply.github.com>
2023-07-05 22:50:27 +0000

    added displayscan which uses dcmread

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-from flask import Flask
-from flask import render_template
+import matplotlib.pyplot as plt
+import os
+from flask import Flask, flash, request, redirect, url_for
+from flask import render_template, send_from_directory
 from flask import jsonify
 
+from pydicom import dcmread
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = {'dcm'}
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.add_url_rule("/uploads/<name>", endpoint="download_file", build_only=True)
 
 JOBS = [
   {
@@ -39,6 +49,54 @@ def hello_world():
 @app.route("/api/jobs")
 def list_jobs():
   return jsonify(JOBS)
+
+
+def allowed_file(filename):
+  return '.' in filename and \
+         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+  if request.method == 'POST':
+    # check if the post request has the file part
+    if 'file' not in request.files:
+      flash('No file part')
+      return redirect(request.url)
+    file = request.files['file']
+    # If the user does not select a file, the browser submits an
+    # empty file without a filename.
+    if file.filename == '':
+      flash('No selected file')
+      return redirect(request.url)
+    if file and allowed_file(file.filename):
+      filename = secure_filename(file.filename)
+      file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+      # ds = dcmread(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+      # patient_id = ds.PatientID
+      return display_file(filename)
+
+  return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+
+@app.route('/uploads/<name>')
+def display_file(name):
+  ds = dcmread(os.path.join(app.config['UPLOAD_FOLDER'], name))
+  patient_id = ds.PatientID
+  # arr = ds.pixel_array
+  # plt.imshow(arr, cmap="gray")
+  # display = plt.show()
+  return render_template("displayscan.html", scan=ds, patient=patient_id)
+
+  # return send_from_directory(app.config["UPLOAD_FOLDER"], name)
 
 
 if __name__ == "__main__":
